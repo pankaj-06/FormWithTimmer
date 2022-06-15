5da885e7e0d730c111f6350d25f4f54d006c33ab
pankaj-06 <you@example.com>
2022-06-15 23:59:41 +0530

    added form and progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserDetailsForm from './Pages/UserForm';
+import { Button, Typography } from '@mui/material';
 
 function App() {
+  const [time, setTime] = useState<number>(0)
+
+  useEffect(() => {
+    if (time === 0) {
+      var timeleft = 300;
+      var downloadTimer = setInterval(function () {
+        if (timeleft <= 0) {
+          clearInterval(downloadTimer);
+        }
+        (document.getElementById("progressBar")! as any).value = 300 - timeleft;
+        setTime(300 - timeleft);
+        timeleft -= 1;
+      }, 1000);
+    }
+
+  }, [time])
+
   return (
     <div className="App">
-      <UserDetailsForm />
+      <div className='app-header'>
+        <Typography variant="h6" gutterBottom style={{ margin: "5px" }}>
+          Add User Details
+        </Typography>
+       
+          <progress className='progress' value="0" max="300" id="progressBar" ></progress>
+        
+      </div>
+      {
+        time === 300 ? (<div className='time-over-conatiner'>
+          <div>TIME IS OVER</div>
+          <Button color="primary" onClick={() => {
+            setTime(0)
+          }}>Reset</Button>
+        </div>) : <UserDetailsForm />
+      }
     </div>
   );
 }
---
+++ src/Massages/messages.ts
@@ -0,0 +1,5 @@
+export const messages = {
+    required: "This field is required.",
+    validEmail:"Please enter valid email.",
+    validName:"Please enter valid name(Alphabets only)."
+}
